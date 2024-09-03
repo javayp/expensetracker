ec34100dbdb12518762ab9e6264bfead366cc1cd
“javayp” <prashanth.yp89@gmail.com>
2024-09-04 02:14:03 +0530

    adding annotation to method

--- src/main/java/com/app/penpaid/api/IndexApi.java
+++ src/main/java/com/app/penpaid/api/IndexApi.java
@@ -1,10 +1,12 @@
 package com.app.penpaid.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
 public class IndexApi {
 
+    @GetMapping
     public String index() {
         return "Hello World!";
     }
