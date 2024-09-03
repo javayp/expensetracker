56b7a3ddf82b57d98c062fbd67583a7451e5b8e3
“javayp” <prashanth.yp89@gmail.com>
2024-09-04 02:03:28 +0530

    Adding method in api

--- src/main/java/com/app/penpaid/api/IndexApi.java
+++ src/main/java/com/app/penpaid/api/IndexApi.java
@@ -4,4 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
 public class IndexApi {
+
+    public String index() {
+        return "Hello World!";
+    }
 }
