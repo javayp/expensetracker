14530d715b62224e6956b9d8673f767c4cf7c852
“javayp” <prashanth.yp89@gmail.com>
2024-09-04 01:45:30 +0530

    Fresh and first commit

---
+++ src/main/java/com/app/penpaid/PenpaidApplication.java
@@ -0,0 +1,13 @@
+package com.app.penpaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenpaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenpaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/penpaid/PenpaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.penpaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenpaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
